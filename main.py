e3d43b7d48a15024cd93dec6b485b0a918242fc7
Moussaa <155332027+slybepo@users.noreply.github.com>
2024-03-27 17:14:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,18 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 async def on_ready():
     print(f'We have logged in as {bot.user}')
     await bot.change_presence(activity=discord.Streaming(name='CosmoS menu', url='https://saphireshop.xyz/'))
-bot = Bot(token=os.environ.get('TOKEN'))
-
-bot.run(bot)
+try:
+  token = os.getenv("TOKEN") or ""
+  if token == "":
+    raise Exception("Please add your token to the Secrets pane.")
+  client.run(token)
+except discord.HTTPException as e:
+    if e.status == 429:
+        print(
+            "The Discord servers denied the connection for making too many requests"
+        )
+        print(
+            "Get help from https://stackoverflow.com/questions/66724687/in-discord-py-how-to-solve-the-error-for-toomanyrequests"
+        )
+    else:
+        raise e
